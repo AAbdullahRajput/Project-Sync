7c9cd4c1174ffd281a3990aa3097d07b41e7c026
Ahmad Abdullah <abdullahhrajput4943@gmail.com>
2026-05-22 19:08:47 +0500

    fix: projectController - replace next() with direct res.json for Express 5

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -1,19 +1,19 @@
 const Notification = require('../models/Notification');
 
 // Get all notifications for current user newest first
-const getNotifications = async (req, res, next) => {
+const getNotifications = async (req, res) => {
   try {
     const notifications = await Notification.find({ recipient: req.user._id })
       .populate('sender', 'name avatar')
       .sort('-createdAt');
     res.json(notifications);
   } catch (error) {
-    next(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
 // Mark all notifications as read
-const markAllRead = async (req, res, next) => {
+const markAllRead = async (req, res) => {
   try {
     await Notification.updateMany(
       { recipient: req.user._id, isRead: false },
@@ -21,17 +21,17 @@ const markAllRead = async (req, res, next) => {
     );
     res.json({ message: 'All notifications marked as read' });
   } catch (error) {
-    next(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
 // Delete a single notification
-const deleteNotification = async (req, res, next) => {
+const deleteNotification = async (req, res) => {
   try {
     await Notification.findByIdAndDelete(req.params.id);
     res.json({ message: 'Notification deleted' });
   } catch (error) {
-    next(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
